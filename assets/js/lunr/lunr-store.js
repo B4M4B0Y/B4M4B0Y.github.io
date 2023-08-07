a0d400b85fe4417f5a357bc2d933c058c5bfca8c
Florian Hennig <florian.hennig@stud.hs-coburg.de>
2023-08-07 10:39:13 +0200

    favicon

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,11 +3,11 @@ var store = [{
         "excerpt":"Writeup zur Inject Maschine von Hack The Box Schwierigkeit - Easy Wir fangen an mit einem nmap-scan Folgender Befehl wird ausgeführt: nmap -p- -A 10.129.177.49 mit -p- scant nmap alle 65 535 Ports mit -A führen wir einen “agressive-scan” aus, dieser kann ggf. das Betriebssystem des Ziels erkennen, Versionsinformationen zu...","categories": [],
         "tags": [],
         "url": "/Hack-The-Box-Inject-Writeup/",
-        "teaser": "/assets/images/writeups/inject/teaser.jpg"
+        "teaser": "/assets/writeups/inject/teaser.jpg"
       },{
         "title": "Hack The Box Agile Writeup",
         "excerpt":"Writeup zur Agile Maschine von Hack The Box Schwierigkeit - Medium Nmap scan Nmap scan report for 10.129.166.121 Host is up (0.026s latency). Not shown: 65533 closed tcp ports (conn-refused) PORT STATE SERVICE VERSION 22/tcp open ssh OpenSSH 8.9p1 Ubuntu 3ubuntu0.1 (Ubuntu Linux; protocol 2.0) | ssh-hostkey: | 256 f4bcee21d71f1aa26572212d5ba6f700...","categories": [],
         "tags": [],
         "url": "/Hack-The-Box-Agile-Writeup/",
-        "teaser": "/assets/images/writeups/agile/teaser.png"
+        "teaser": "/assets/writeups/agile/teaser.png"
       }]
